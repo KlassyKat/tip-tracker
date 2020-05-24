73f65114b2e16d9c823b9327378cd4e31bbe420b
GabeAlexander <43545418+GabeAlexander@users.noreply.github.com>
2020-05-23 22:22:38 -0400

    connect aws amplify

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
+const STATIC_CACHE = 'site-static';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/css/main.css'
+]
+
 // Install Servie Worker
 self.addEventListener('install', (evt) => {
-    console.log("Service Worker installed.")
+    // Handle async
+    evt.waitUntil(
+        // Cache essentials
+        caches.open(STATIC_CACHE)
+        .then(cache => {
+            cache.addAll(ASSETS)
+        })
+    )
 });
 
 // Activate Service Worker
@@ -10,5 +25,10 @@ self.addEventListener('activate', (evt) => {
 
 // Fetch Events
 self.addEventListener('fetch', (evt) => {
-    console.log(evt)
+    evt.respondWith(
+        caches.match(evt.request)
+            .then(cacheRes => {
+                return cacheRes || fetch(evt.request);
+            })
+    )
 });
